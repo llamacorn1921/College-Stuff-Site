8334f1e8a85cf8b9bb9d67e79773d0b76ee07c9a
Llama <None@of.yours>
2022-10-04 23:22:02 -0400

    Build | Tue Oct  4 23:22:02 EDT 2022

--- src/config.editor.js
+++ src/config.editor.js
@@ -2,7 +2,6 @@ requirejs.config({
 	baseUrl: "build/",
 	paths: {
 		vs: "//cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.34.0/min/vs/editor/editor.main.min",
-		jquery: "//ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min",
 		lib: "lib"
 	},
 	waitSeconds: 5
--- src/pages/page.editor.js
+++ src/pages/page.editor.js
@@ -1,8 +1,4 @@
 define((require) => {
-	const $ = require('jquery');
 	const lib = require('lib');
 	const vs = require("vs")
-	$(function() {
-		console.log( "ready!" );
-	});
 })
