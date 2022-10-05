def76d81d06d305006d80339ead46144dce3f3a7
Llama <None@of.yours>
2022-10-04 23:18:09 -0400

    cd

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 npx glob-uglifyjs -c build.json
 git checkout site
-git checkout main build/ *.html
-git add build/ *.html
+git checkout main build/
+git add build/
 git commit -m "Build | $(date)"
 git push origin site
